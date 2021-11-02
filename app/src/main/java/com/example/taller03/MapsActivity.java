b1a5c6a7adce65634882185a1a64532c9eb318a5
Nelson Mosquera <nelson.a.mosquera@gmail.com>
2021-11-02 00:40:13 -0500

    Creates map with location

---
+++ app/src/main/java/com/example/taller03/MapsActivity.java
@@ -0,0 +1,286 @@
+package com.example.taller03;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+import androidx.fragment.app.FragmentActivity;
+
+import android.Manifest;
+import android.app.Activity;
+import android.content.Intent;
+import android.content.IntentSender;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
+
+import com.google.android.gms.common.api.ApiException;
+import com.google.android.gms.common.api.CommonStatusCodes;
+import com.google.android.gms.common.api.ResolvableApiException;
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationCallback;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationResult;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.location.LocationSettingsRequest;
+import com.google.android.gms.location.LocationSettingsResponse;
+import com.google.android.gms.location.LocationSettingsStatusCodes;
+import com.google.android.gms.location.SettingsClient;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+import com.example.taller03.databinding.ActivityMapsBinding;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.Task;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.logging.Logger;
+
+public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
+
+    private static final String LOCATIONS_FILE = "locations.json";
+    private GoogleMap mMap;
+    private ActivityMapsBinding binding;
+    private JSONArray json_locations_file;
+    ArrayList<LocationJSON> json_locationJSONS = new ArrayList<>();
+    private Logger logger = Logger.getLogger("TAG");
+    boolean first_location = true;
+
+    //Variables de permisos
+    private final int LOCATION_PERMISSION_ID = 103;
+    public static final int REQUEST_CHECK_SETTINGS = 201;
+    String locationPerm = Manifest.permission.ACCESS_FINE_LOCATION;
+
+    //Variables de localizacion
+    private FusedLocationProviderClient mFusedLocationClient;
+    private LocationRequest mLocationRequest;
+    private LocationCallback mLocationCallback;
+    Location mCurrentLocation;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        binding = ActivityMapsBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        try {
+            JSONObject jsonFile = loadLocationsByJSON();
+            json_locations_file = jsonFile.getJSONArray("locations");
+
+            for(int i = 0; i < json_locations_file.length(); i++){
+                LocationJSON temp = new LocationJSON();
+                temp.setName(json_locations_file.getJSONObject(i).getString("name"));
+                temp.setLatitude(json_locations_file.getJSONObject(i).getDouble("latitude"));
+                temp.setLongitude(json_locations_file.getJSONObject(i).getDouble("longitude"));
+                json_locationJSONS.add(temp);
+
+            }
+            //Log.i(json_locations.getJSONObject(0).getString("name"));
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
+        mLocationRequest = createLocationRequest();
+        mFusedLocationClient = LocationServices.getFusedLocationProviderClient(this);
+        requestPermission(this, Manifest.permission.ACCESS_FINE_LOCATION, "El permiso es necesario para acceder a la localizacion", LOCATION_PERMISSION_ID);
+
+        mLocationCallback = new LocationCallback() {
+            @Override
+            public void onLocationResult(LocationResult locationResult) {
+                Location location = locationResult.getLastLocation();
+                Log.i("LOCATION", "LOCATION CALLBACK" + location);
+                if (location != null) {
+                    mCurrentLocation = location;
+                    paintMyLocation();
+                    first_location = false;
+                    /*logger.info(String.valueOf(location.getLatitude()));
+                    logger.info(String.valueOf(location.getLongitude()));
+                    logger.info(String.valueOf(location.getAltitude()));*/
+                }
+            }
+        };
+
+        turnOnLocationAndStartUpdates();
+
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+    }
+    @Override
+    protected void onResume() {
+        super.onResume();
+        first_location = true;
+        startLocationUpdates();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        stopLocationUpdates();
+    }
+    /**
+     * Manipulates the map once available.
+     * This callback is triggered when the map is ready to be used.
+     * This is where we can add markers or lines, add listeners or move the camera. In this case,
+     * we just add a marker near Sydney, Australia.
+     * If Google Play services is not installed on the device, the user will be prompted to install
+     * it inside the SupportMapFragment. This method will only be triggered once the user has
+     * installed Google Play services and returned to the app.
+     */
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+
+        // Add a marker in Sydney and move the camera
+        //Permitir gestos
+        mMap.getUiSettings().setAllGesturesEnabled(true);
+        //zoom buttons
+        mMap.getUiSettings().setZoomControlsEnabled(true);
+        for (LocationJSON loc: json_locationJSONS) {
+            LatLng json_loc = new LatLng(loc.getLatitude(),loc.getLongitude());
+            mMap.addMarker(new MarkerOptions().position(json_loc).title(loc.getName()).icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_GREEN)));
+        }
+
+    }
+
+    public String loadJSONFromAsset(String assetName) {
+        String json = null;
+        try {
+            InputStream is = this.getAssets().open(assetName);
+            int size = is.available();
+            byte[] buffer = new byte[size];
+            is.read(buffer);
+            is.close();
+            json = new String(buffer, "UTF-8");
+        } catch (IOException ex) {
+            ex.printStackTrace();
+            return null;
+        }
+        return json;
+    }
+
+    public JSONObject loadLocationsByJSON() throws JSONException {
+        return new JSONObject(loadJSONFromAsset(LOCATIONS_FILE));
+    }
+
+
+    //Location section
+
+    public void paintMyLocation() {
+        LatLng my_location = new LatLng(mCurrentLocation.getLatitude(), mCurrentLocation.getLongitude());
+        mMap.addMarker(new MarkerOptions().position(my_location).title("My location").icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_VIOLET)));
+        if(first_location){
+            mMap.moveCamera(CameraUpdateFactory.newLatLng(my_location));
+            mMap.moveCamera(CameraUpdateFactory.zoomTo(15));
+        }
+
+    }
+
+    private void startLocationUpdates() {
+        if (ContextCompat.checkSelfPermission(this,
+                Manifest.permission.ACCESS_FINE_LOCATION)
+                == PackageManager.PERMISSION_GRANTED) {
+            mFusedLocationClient.requestLocationUpdates(mLocationRequest,
+                    mLocationCallback, null);
+        }
+    }
+
+    private void stopLocationUpdates() {
+        mFusedLocationClient.removeLocationUpdates(mLocationCallback);
+    }
+
+    private LocationRequest createLocationRequest() {
+        LocationRequest mLocationRequest = new LocationRequest();
+        mLocationRequest.setInterval(10000);
+        mLocationRequest.setFastestInterval(5000);
+        mLocationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+        return mLocationRequest;
+    }
+
+    private void requestPermission(Activity context, String permiso, String justificacion, int idCode) {
+        if (ContextCompat.checkSelfPermission(context, permiso) != PackageManager.PERMISSION_GRANTED) {
+            if (ActivityCompat.shouldShowRequestPermissionRationale(context, permiso)) {
+                Toast.makeText(context, justificacion, Toast.LENGTH_SHORT).show();
+            }
+            ActivityCompat.requestPermissions(context, new String[]{permiso}, idCode);
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        switch (requestCode) {
+            case LOCATION_PERMISSION_ID: {
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    Toast.makeText(this, "Ya hay permiso para acceder a la localizacion", Toast.LENGTH_LONG).show();
+                    turnOnLocationAndStartUpdates();
+                } else {
+                    Toast.makeText(this, "No hay Permiso :(", Toast.LENGTH_LONG).show();
+                }
+                return;
+            }
+        }
+    }
+
+    private void turnOnLocationAndStartUpdates() {
+        LocationSettingsRequest.Builder builder =
+                new LocationSettingsRequest.Builder().addLocationRequest(mLocationRequest);
+        SettingsClient client = LocationServices.getSettingsClient(this);
+        Task<LocationSettingsResponse> task =
+                client.checkLocationSettings(builder.build());
+        task.addOnSuccessListener(this, locationSettingsResponse -> {
+            startLocationUpdates(); //Todas las condiciones para recibir localizaciones
+        });
+        task.addOnFailureListener(this, new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                int statusCode = ((ApiException) e).getStatusCode();
+                switch (statusCode) {
+                    case CommonStatusCodes.RESOLUTION_REQUIRED:
+                        // Location settings are not satisfied, but this can be fixed by showing the user a dialog.
+                        try {
+                            // Show the dialog by calling startResolutionForResult(), and check the result in onActivityResult().
+                            ResolvableApiException resolvable = (ResolvableApiException) e;
+                            resolvable.startResolutionForResult(MapsActivity.this, REQUEST_CHECK_SETTINGS);
+                        } catch (IntentSender.SendIntentException sendEx) {
+                            // Ignore the error.
+                        }
+                        break;
+                    case LocationSettingsStatusCodes.SETTINGS_CHANGE_UNAVAILABLE:
+                        // Location settings are not satisfied. No way to fix the settings so we won't show the dialog.
+                        break;
+                }
+            }
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        switch (requestCode) {
+            case REQUEST_CHECK_SETTINGS: {
+                if (resultCode == RESULT_OK) {
+                    startLocationUpdates();
+                } else {
+                    Toast.makeText(this, "Sin acceso a localizacion, hardware deshabilitado!", Toast.LENGTH_LONG).show();
+                }
+            }
+        }
+
+    }
+
+}
